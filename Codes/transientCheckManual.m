d0e392ce41c7ea3e369e2d259a03c80da8fe07a6
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-01 16:21:02 -0600

    working through data import and table concatenation

--- Codes/transientCheckManual.m
+++ Codes/transientCheckManual.m
@@ -2,8 +2,8 @@ clc
 close all
 clear all
 
-%% Load Data
-start = 8;
+%% Load Folder Structure
+start = 7;
 nRuns = 9;
 
 loc = 'C:\Users\drewm\Documents\2.0 MSc\2.0 Simulations\';
@@ -23,4 +23,43 @@ for i = start+1:nRuns
     allTimes(rows).datenum = tempDir(3).datenum;
 end
 
-%% Setup data for analysis
+latest_time = allTimes(end).name;
+
+vars = [9, 13, 19];
+data_headers = ["time", "jetOutletFlow", "tubeInletFlow","probe_k", "probe_Ma", ...
+    "probe_p", "probe_rho", "probe_T", "probe_U", "probe_UMean", "probe_UPrime2Mean" ];
+
+%% Load Data
+for i = vars
+    fold_name = allFolder(i).name;
+    files = dir(append(append(loc, sim,'\postProcessing-',num2str(nRuns),'\',fold_name,'\',num2str(latest_time))));
+
+    for j = 3:height(files)
+        data_name_holder = append(append(loc, sim,'\postProcessing-',num2str(nRuns),'\',fold_name,'\',num2str(latest_time),'/',files(j).name));
+        temp_data = readtable(data_name_holder);
+        % Assign time var
+        if i==9                                 
+            postProcessing_data(:,1) = temp_data(:,1);
+        end
+        disp(files(j).name)
+        cols = width(temp_data);
+        % Fix for same header names
+        if string(temp_data.Properties.VariableNames{2}) == "sum_phi_"
+            temp_data.Properties.VariableNames{2} = fold_name;
+        elseif (string(temp_data.Properties.VariableNames{2}) == "Var2") && (files(j).name(1)~='U')
+            temp_data.Properties.VariableNames{2} = append('probe1250_',files(j).name);
+        elseif (files(j).name(1)~='U')
+            keyboard 
+        end
+        % Fix to concat tables of diff height
+        if height(temp_data)>height(postProcessing_data)
+            postProcessing_data = cat(2, postProcessing_data, temp_data(1:height(postProcessing_data), 2));
+        else
+            postProcessing_data = cat(2, postProcessing_data, temp_data(:, 2:end));
+        end
+
+    end
+
+
+end
+
