6b6530c5ef6c64c7f6549eb03d08bb4bafed5bb4
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-01 23:25:48 -0600

    finished postProcessing data structure

--- Codes/transientCheckManual.m
+++ Codes/transientCheckManual.m
@@ -35,27 +35,56 @@ for i = vars
     files = dir(append(append(loc, sim,'\postProcessing-',num2str(nRuns),'\',fold_name,'\',num2str(latest_time))));
 
     for j = 3:height(files)
-        data_name_holder = append(append(loc, sim,'\postProcessing-',num2str(nRuns),'\',fold_name,'\',num2str(latest_time),'/',files(j).name));
-        temp_data = readtable(data_name_holder);
-        % Assign time var
-        if i==9                                 
-            postProcessing_data(:,1) = temp_data(:,1);
+        %Skip Prime2MeanData, Not important right now
+        if string(files(j).name) == "UPrime2Mean"
+            continue
         end
+
+        % Get variable name and import data
+        data_name_holder = append(append(loc, sim,'\postProcessing-',num2str(nRuns),'\',fold_name,'\',num2str(latest_time),'/',files(j).name));
         disp(files(j).name)
+        temp_data = readtable(data_name_holder);
+
+        % Assign time var
+        if i==9
+            pp_data(:,1) = temp_data(:,1);     
+            pp_data.Properties.VariableNames{1} = 'time';
+        end
         cols = width(temp_data);
-        % Fix for same header names
+
+        % Assign header names and handle datatype conversions
         if string(temp_data.Properties.VariableNames{2}) == "sum_phi_"
             temp_data.Properties.VariableNames{2} = fold_name;
+
         elseif (string(temp_data.Properties.VariableNames{2}) == "Var2") && (files(j).name(1)~='U')
             temp_data.Properties.VariableNames{2} = append('probe1250_',files(j).name);
-        elseif (files(j).name(1)~='U')
-            keyboard 
+
+        elseif (files(j).name(1)=='U') && (string(files(j).name)~="UPrime2Mean")
+            if string(files(j).name)=="UMean"
+                temp_data_conv(1:height(temp_data),2) = str2double(erase(string(temp_data{:,2}), '('));
+                temp_data_conv(1:height(temp_data),3) = temp_data{:,3};
+                temp_data_conv(1:height(temp_data),4) = str2double(erase(string(temp_data{:,4}), ')'));
+            else
+            temp_data_conv(:,2) = str2double(erase(string(temp_data{:,2}), '('));
+            temp_data_conv(:,3) = temp_data{:,3};
+            temp_data_conv(:,4) = str2double(erase(string(temp_data{:,4}), ')'));
+            end
         end
-        % Fix to concat tables of diff height
-        if height(temp_data)>height(postProcessing_data)
-            postProcessing_data = cat(2, postProcessing_data, temp_data(1:height(postProcessing_data), 2));
+
+        % Fix to concat tables and handle array length di8screpancies
+        if (height(temp_data)>height(pp_data)) || exist('temp_data_conv')
+            if (files(j).name(1)=='U')
+                pp_data = cat(2, pp_data, table(temp_data_conv(1:height(pp_data), 2)));
+                pp_data.Properties.VariableNames{end} = append('probe1250_',files(j).name(:)', '_1');
+                pp_data = cat(2, pp_data, table(temp_data_conv(1:height(pp_data), 3)));
+                pp_data.Properties.VariableNames{end} = append('probe1250_',files(j).name(:)', '_2');
+                pp_data = cat(2, pp_data, table(temp_data_conv(1:height(pp_data), 4)));
+                pp_data.Properties.VariableNames{end} = append('probe1250_',files(j).name(:)', '_3');
+            else
+                pp_data = cat(2, pp_data, temp_data(1:height(pp_data), 2));
+            end
         else
-            postProcessing_data = cat(2, postProcessing_data, temp_data(:, 2:end));
+            pp_data = cat(2, pp_data, temp_data(:, 2:end));
         end
 
     end
