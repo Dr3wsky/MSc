2d7972a05a24b19938bec025fd46f54a58143c8f
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-08 11:40:30 -0600

    adding Reynolds Stress var to data pull

--- Codes/transientCheckManual.m
+++ Codes/transientCheckManual.m
@@ -4,8 +4,8 @@ clear all
 format longg
 
 %% Load Folder Structure
-start = 7;
-nRuns = 9;
+start = 9;
+nRuns = 10;
 
 loc = 'C:\Users\drewm\Documents\2.0 MSc\2.0 Simulations\';
 sim = 'RSM_LRR';
@@ -57,30 +57,55 @@ for i = vars
         if string(temp_data.Properties.VariableNames{2}) == "sum_phi_"
             temp_data.Properties.VariableNames{2} = fold_name;
 
-        elseif (string(temp_data.Properties.VariableNames{2}) == "Var2") && (files(j).name(1)~='U')
-            temp_data.Properties.VariableNames{2} = append('probe1250_',files(j).name);
+        elseif (string(temp_data.Properties.VariableNames{2}) == "Var2") && (files(j).name(1)~='U' && files(j).name(1)~='R')
+            temp_data.Properties.VariableNames{2} = append(files(j).name, '_probe1250');
 
         elseif (files(j).name(1)=='U') && (string(files(j).name)~="UPrime2Mean")
             if string(files(j).name)=="UMean"
-                temp_data_conv(1:height(temp_data),2) = str2double(erase(string(temp_data{:,2}), '('));
-                temp_data_conv(1:height(temp_data),3) = temp_data{:,3};
-                temp_data_conv(1:height(temp_data),4) = str2double(erase(string(temp_data{:,4}), ')'));
+                temp_data_convU(1:height(temp_data),2) = str2double(erase(string(temp_data{:,2}), '('));
+                temp_data_convU(1:height(temp_data),3) = temp_data{:,3};
+                temp_data_convU(1:height(temp_data),4) = str2double(erase(string(temp_data{:,4}), ')'));
             else
-            temp_data_conv(:,2) = str2double(erase(string(temp_data{:,2}), '('));
-            temp_data_conv(:,3) = temp_data{:,3};
-            temp_data_conv(:,4) = str2double(erase(string(temp_data{:,4}), ')'));
+                temp_data_convU(:,2) = str2double(erase(string(temp_data{:,2}), '('));
+                temp_data_convU(:,3) = temp_data{:,3};
+                temp_data_convU(:,4) = str2double(erase(string(temp_data{:,4}), ')'));
+            end
+
+        elseif (files(j).name(1)=='R')
+            if string(files(j).name)=="RMean"
+                temp_data_convR(1:height(temp_data),2) = str2double(erase(string(temp_data{:,2}), '('));
+                temp_data_convR(1:height(temp_data),3:6) = temp_data{:,3:6};
+                temp_data_convR(1:height(temp_data),7) = str2double(erase(string(temp_data{:,7}), ')'));
+            else
+                temp_data_convR(:,2) = str2double(erase(string(temp_data{:,2}), '('));
+                temp_data_convR(:,3:6) = temp_data{:,3:6};
+                temp_data_convR(:,7) = str2double(erase(string(temp_data{:,7}), ')'));
             end
         end
 
         % Fix to concat tables and handle array length di8screpancies
-        if (height(temp_data)>height(pp_data)) || exist('temp_data_conv')
+        if (height(temp_data)>height(pp_data)) || exist('temp_data_convU') || exist('temp_data_convR')
             if (files(j).name(1)=='U')
-                pp_data = cat(2, pp_data, table(temp_data_conv(1:height(pp_data), 2)));
-                pp_data.Properties.VariableNames{end} = append('probe1250_',files(j).name(:)', '_1');
-                pp_data = cat(2, pp_data, table(temp_data_conv(1:height(pp_data), 3)));
-                pp_data.Properties.VariableNames{end} = append('probe1250_',files(j).name(:)', '_2');
-                pp_data = cat(2, pp_data, table(temp_data_conv(1:height(pp_data), 4)));
-                pp_data.Properties.VariableNames{end} = append('probe1250_',files(j).name(:)', '_3');
+                pp_data = cat(2, pp_data, table(temp_data_convU(1:height(pp_data), 2)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_1_','probe1250');
+                pp_data = cat(2, pp_data, table(temp_data_convU(1:height(pp_data), 3)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_2_', 'probe1250');
+                pp_data = cat(2, pp_data, table(temp_data_convU(1:height(pp_data), 4)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_3_','probe1250');
+            elseif (files(j).name(1)=='R')
+                pp_data = cat(2, pp_data, table(temp_data_convR(1:height(pp_data), 2)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_xx_', 'probe1250');
+                pp_data = cat(2, pp_data, table(temp_data_convR(1:height(pp_data), 3)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_yy_','probe1250');
+                pp_data = cat(2, pp_data, table(temp_data_convR(1:height(pp_data), 4)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_zz_','probe1250');
+                pp_data = cat(2, pp_data, table(temp_data_convR(1:height(pp_data), 5)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_xy_','probe1250');
+                pp_data = cat(2, pp_data, table(temp_data_convR(1:height(pp_data), 6)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_yz_','probe1250');
+                pp_data = cat(2, pp_data, table(temp_data_convR(1:height(pp_data), 7)));
+                pp_data.Properties.VariableNames{end} = append(files(j).name(:)', '_xz_','probe1250');
+
             else
                 pp_data = cat(2, pp_data, temp_data(1:height(pp_data), 2));
             end
@@ -93,15 +118,15 @@ end
 %% Transient Data Analysis
 format longg
 
-% Variable of interest
-time_var = pp_data.time;
-y_vars = [pp_data.probe1250_U_1, pp_data.probe1250_U_2, pp_data.probe1250_UMean_2, pp_data.probe1250_UMean_1, pp_data.probe1250_k, pp_data.jetOutletFlow];
-
-for i = 1:length(y_vars) 
-    % Find periodicity
-    [pks,locs] = findpeaks(y_vars(:,i), time_var);
-    period(i) = max(diff(locs));
-end
+% % Variable of interest
+% time_var = pp_data.time;
+% y_vars = [pp_data.probe1250_U_1, pp_data.probe1250_U_2, pp_data.probe1250_UMean_2, pp_data.probe1250_UMean_1, pp_data.probe1250_k, pp_data.jetOutletFlow];
+% 
+% for i = 1:length(y_vars) 
+%     % Find periodicity
+%     [pks,locs] = findpeaks(y_vars(:,i), time_var);
+%     period(i) = max(diff(locs));
+% end
 
 
 
