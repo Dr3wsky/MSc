fb284f462245b2776895e7845ee75c8140fe53e7
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-17 22:01:24 -0600

    updataing .C with MachNo funcs

--- OpenFOAM/kOmegaSSTdilationDissipation/kOmegaSSTdilationDissipation.C
+++ OpenFOAM/kOmegaSSTdilationDissipation/kOmegaSSTdilationDissipation.C
@@ -26,180 +26,185 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "kOmegaSSTdilationDissipation.H"
+#include "MachNo.H"
+#include "fluidThermo.H"
+#include "fieldExpression.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
-namespace RASModels
-{
-
-// * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
-
-template<class BasicTurbulenceModel>
-void kOmegaSSTdilationDissipation<BasicTurbulenceModel>::correctNut(const volScalarField& S2)
-{
-    // Correct the turbulence viscosity
-    kOmegaSSTBase<eddyViscosity<RASModel<BasicTurbulenceModel>>>::correctNut
-    (
-        S2
-    );
-
-    // Correct the turbulence thermal diffusivity
-    BasicTurbulenceModel::correctNut();
+    namespace functionObjects
+    {
+        defineTypeNameAndDebug(MachNo, 0);
+        addToRunTimeSelectionTable(functionObject, MachNo, dictionary);
+    }
 }
-
-
-template<class BasicTurbulenceModel>
-void kOmegaSSTdilationDissipation<BasicTurbulenceModel>::correctNut()
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+bool Foam::functionObjects::MachNo::calc()
 {
-    correctNut(2*magSqr(symm(fvc::grad(this->U_))));
-}
+    if (
+        foundObject<volVectorField>(fieldName_) && foundObject<fluidThermo>(fluidThermo::dictName))
+    {
+        const fluidThermo &thermo =
+            lookupObject<fluidThermo>(fluidThermo::dictName);
 
+        const volVectorField &U = lookupObject<volVectorField>(fieldName_);
+
+        return store(
+            resultName_,
+            mag(U) / sqrt(thermo.gamma() * thermo.p() / thermo.rho()));
+    }
+
+    return false;
+}
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-template<class BasicTurbulenceModel>
-kOmegaSSTdilationDissipation<BasicTurbulenceModel>::kOmegaSSTdilationDissipation
-(
-    const alphaField& alpha,
-    const rhoField& rho,
-    const volVectorField& U,
-    const surfaceScalarField& alphaRhoPhi,
-    const surfaceScalarField& phi,
-    const transportModel& transport,
-    const word& propertiesName,
-    const word& type
-)
-:
-    kOmegaSSTBase<eddyViscosity<RASModel<BasicTurbulenceModel>>>
-    (
-        type,
-        alpha,
-        rho,
-        U,
-        alphaRhoPhi,
-        phi,
-        transport,
-        propertiesName
-    )
+Foam::functionObjects::MachNo::MachNo(
+    const word &name,
+    const Time &runTime,
+    const dictionary &dict)
+    : fieldExpression(name, runTime, dict, "U")
 {
-    if (type == typeName)
-    {
-        this->printCoeffs(type);
-    }
+    setResultName("Ma", "U");
 }
 
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-template<class BasicTurbulenceModel>
-void kOmegaSSTdilationDissipation<BasicTurbulenceModel>::correct()
+namespace Foam
 {
-    if (!this->turbulence_)
+
+    namespace RASModels
     {
-        return;
-    }
-    
-    // Output statements to confirm use
-    Info << "----------------------------------------------" << endl;
-    Info << "RUNNING kONegaSSTdilationDissipation" << endl;
-    Info << "----------------------------------------------" << endl;
 
-    // Local references
-    const alphaField& alpha = this->alpha_;
-    const rhoField& rho = this->rho_;
-    const surfaceScalarField& alphaRhoPhi = this->alphaRhoPhi_;
-    const volVectorField& U = this->U_;
-    volScalarField& nut = this->nut_;
-    fv::options& fvOptions(fv::options::New(this->mesh_));
-    
-    
+        // * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
 
-    BasicTurbulenceModel::correct();
+        template <class BasicTurbulenceModel>
+        void kOmegaSSTdilationDissipation<BasicTurbulenceModel>::correctNut(const volScalarField &S2)
+        {
+            // Correct the turbulence viscosity
+            kOmegaSSTBase<eddyViscosity<RASModel<BasicTurbulenceModel>>>::correctNut(
+                S2);
 
-    volScalarField::Internal divU(fvc::div(fvc::absolute(this->phi(), U)));
+            // Correct the turbulence thermal diffusivity
+            BasicTurbulenceModel::correctNut();
+        }
 
-    tmp<volTensorField> tgradU = fvc::grad(U);
-    volScalarField S2(2*magSqr(symm(tgradU())));
-    volScalarField::Internal GbyNu0
-    (
-        this->type() + ":GbyNu",
-        (tgradU() && dev(twoSymm(tgradU())))
-    );
-    volScalarField::Internal G(this->GName(), nut*GbyNu0);
+        template <class BasicTurbulenceModel>
+        void kOmegaSSTdilationDissipation<BasicTurbulenceModel>::correctNut()
+        {
+            correctNut(2 * magSqr(symm(fvc::grad(this->U_))));
+        }
 
-    // Update omega and G at the wall
-    this->omega_.boundaryFieldRef().updateCoeffs();
+        // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-    volScalarField CDkOmega
-    (
-        (2*this->alphaOmega2_)*(fvc::grad(this->k_) & fvc::grad(this->omega_))/this->omega_
-    );
+        template <class BasicTurbulenceModel>
+        kOmegaSSTdilationDissipation<BasicTurbulenceModel>::kOmegaSSTdilationDissipation(
+            const alphaField &alpha,
+            const rhoField &rho,
+            const volVectorField &U,
+            const surfaceScalarField &alphaRhoPhi,
+            const surfaceScalarField &phi,
+            const transportModel &transport,
+            const word &propertiesName,
+            const word &type)
+            : kOmegaSSTBase<eddyViscosity<RASModel<BasicTurbulenceModel>>>(
+                  type,
+                  alpha,
+                  rho,
+                  U,
+                  alphaRhoPhi,
+                  phi,
+                  transport,
+                  propertiesName)
+        {
+            if (type == typeName)
+            {
+                this->printCoeffs(type);
+            }
+        }
 
-    volScalarField F1(this->F1(CDkOmega));
-    volScalarField F23(this->F23());
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+        template <class BasicTurbulenceModel>
+        void kOmegaSSTdilationDissipation<BasicTurbulenceModel>::correct()
+        {
+            if (!this->turbulence_)
+            {
+                return;
+            }
 
-    {
-        volScalarField::Internal gamma(this->gamma(F1));
-        volScalarField::Internal beta(this->beta(F1));
+            // Local references
+            const alphaField &alpha = this->alpha_;
+            const rhoField &rho = this->rho_;
+            const surfaceScalarField &alphaRhoPhi = this->alphaRhoPhi_;
+            const volVectorField &U = this->U_;
+            // volScalarField &MachTest = this->MachTest_;
+            volScalarField &omega = this->omega_;
+            volScalarField &k = this->k_;
+            volScalarField &nut = this->nut_;
 
-        GbyNu0 = this->GbyNu(GbyNu0, F23(), S2());
+            fv::options &
+                fvOptions(fv::options::New(this->mesh_));
 
-        // Turbulent frequency equation
-        tmp<fvScalarMatrix> omegaEqn
-        (
-            fvm::ddt(alpha, rho, this->omega_)
-          + fvm::div(alphaRhoPhi, this->omega_)
-          - fvm::laplacian(alpha*rho*this->DomegaEff(F1), this->omega_)
-         ==
-            alpha()*rho()*gamma*GbyNu0
-          - fvm::SuSp((2.0/3.0)*alpha()*rho()*gamma*divU, this->omega_)
-          - fvm::Sp(alpha()*rho()*beta*this->omega_(), this->omega_)
-          - fvm::SuSp
-            (
-                alpha()*rho()*(F1() - scalar(1))*CDkOmega()/this->omega_(),
-                this->omega_
-            )
-          + alpha()*rho()*beta*sqr(this->omegaInf_)
-          + this->Qsas(S2(), gamma, beta)
-          + this->omegaSource()
-          + fvOptions(alpha, rho, this->omega_)
-        );
+            // Output statements to confirm use
+            Info << "----------------------------------------------" << endl;
+            Info << "RUNNING kOmegaSSTdilationDissipation" << endl;
+            Info << "----------------------------------------------" << endl;
 
-        omegaEqn.ref().relax();
-        fvOptions.constrain(omegaEqn.ref());
-        omegaEqn.ref().boundaryManipulate(this->omega_.boundaryFieldRef());
-        solve(omegaEqn);
-        fvOptions.correct(this->omega_);
-        bound(this->omega_, this->omegaMin_);
-    }
+            BasicTurbulenceModel::correct();
 
-    // Turbulent kinetic energy equation
-    tmp<fvScalarMatrix> kEqn
-    (
-        fvm::ddt(alpha, rho, this->k_)
-      + fvm::div(alphaRhoPhi, this->k_)
-      - fvm::laplacian(alpha*rho*this->DkEff(F1), this->k_)
-     ==
-        alpha()*rho()*this->Pk(G)
-      - fvm::SuSp((2.0/3.0)*alpha()*rho()*divU, this->k_)
-      - fvm::Sp(alpha()*rho()*this->epsilonByk(F1, tgradU()), this->k_)
-      + alpha()*rho()*this->betaStar_*this->omegaInf_*this->kInf_
-      + this->kSource()
-      + fvOptions(alpha, rho, this->k_)
-    );
+            volScalarField::Internal divU(fvc::div(fvc::absolute(this->phi(), U)));
 
-    tgradU.clear();
+            tmp<volTensorField> tgradU = fvc::grad(U);
+            volScalarField S2(2 * magSqr(symm(tgradU())));
+            volScalarField::Internal GbyNu0(
+                this->type() + ":GbyNu",
+                (tgradU() && dev(twoSymm(tgradU()))));
+            volScalarField::Internal G(this->GName(), nut * GbyNu0);
 
-    kEqn.ref().relax();
-    fvOptions.constrain(kEqn.ref());
-    solve(kEqn);
-    fvOptions.correct(this->k_);
-    bound(this->k_, this->kMin_);
+            // Update omega and G at the wall
+            omega.boundaryFieldRef().updateCoeffs();
 
-    correctNut(S2);
-}
-} // End namespace RASModels
+            volScalarField CDkOmega(
+                (2 * this->alphaOmega2_) * (fvc::grad(k) & fvc::grad(omega)) / omega);
+
+            volScalarField F1(this->F1(CDkOmega));
+            volScalarField F23(this->F23());
+
+            {
+                volScalarField::Internal gamma(this->gamma(F1));
+                volScalarField::Internal beta(this->beta(F1));
+
+                GbyNu0 = this->GbyNu(GbyNu0, F23(), S2());
+
+                // Turbulent frequency equation
+                tmp<fvScalarMatrix> omegaEqn(
+                    fvm::ddt(alpha, rho, omega) + fvm::div(alphaRhoPhi, omega) - fvm::laplacian(alpha * rho * this->DomegaEff(F1), omega) ==
+                    alpha() * rho() * gamma * GbyNu0 - fvm::SuSp((2.0 / 3.0) * alpha() * rho() * gamma * divU, omega) - fvm::Sp(alpha() * rho() * beta * omega(), omega) - fvm::SuSp(alpha() * rho() * (F1() - scalar(1)) * CDkOmega() / omega(), omega) + alpha() * rho() * beta * sqr(this->omegaInf_) + this->Qsas(S2(), gamma, beta) + this->omegaSource() + fvOptions(alpha, rho, omega));
+
+                omegaEqn.ref().relax();
+                fvOptions.constrain(omegaEqn.ref());
+                omegaEqn.ref().boundaryManipulate(omega.boundaryFieldRef());
+                solve(omegaEqn);
+                fvOptions.correct(omega);
+                bound(omega, this->omegaMin_);
+            }
+
+            // Turbulent kinetic energy equation
+            tmp<fvScalarMatrix> kEqn(
+                fvm::ddt(alpha, rho, k) + fvm::div(alphaRhoPhi, k) - fvm::laplacian(alpha * rho * this->DkEff(F1), k) ==
+                alpha() * rho() * this->Pk(G) - fvm::SuSp((2.0 / 3.0) * alpha() * rho() * divU, k) - fvm::Sp(alpha() * rho() * this->epsilonByk(F1, tgradU()), k) + alpha() * rho() * this->betaStar_ * this->omegaInf_ * this->kInf_ + this->kSource() + fvOptions(alpha, rho, k));
+
+            tgradU.clear();
+
+            kEqn.ref().relax();
+            fvOptions.constrain(kEqn.ref());
+            solve(kEqn);
+            fvOptions.correct(k);
+            bound(k, this->kMin_);
+
+            correctNut(S2);
+        }
+    } // End namespace RASModels
 } // End namespace Foam
 
 // ************************************************************************* //
