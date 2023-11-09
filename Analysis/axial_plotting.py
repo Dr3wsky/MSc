85f06de0861d201fc0eae21016ca6c0038601888
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-11-09 11:59:33 -0700

    added plotting skeleton

--- Analysis/axial_plotting.py
+++ Analysis/axial_plotting.py
@@ -17,6 +17,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load and handle data
 # ----------------------------------------------------------------------------   
@@ -33,10 +34,13 @@ with open('exp_data.pkl', 'rb') as file:
     file.close()
     
 # Assign experimental data range and variable
-exp_var = 'u_centreline'
+exp_var = 'v_centreline'
 exp_xd = exp_data['x'][0] / 6.4
-exp_start = np.where(exp_data[f'{exp_var}'] > 0)[0][0]
-exp_end = np.where(exp_data[f'{exp_var}'][exp_start:] < 100)[0][0]
+# exp_start = np.where(exp_data[f'{exp_var}'] != 0)[0][0]
+# exp_end = np.where(exp_data[f'{exp_var}'][exp_start:] < 100)[0][0]
+exp_start = 10
+exp_end = len(exp_xd)
+
     
 sim_data = {}
 # Load simulation data
@@ -46,36 +50,42 @@ for i in range(len(sim_names)):
 # Assign plotting range and variable of interest
 sim_xd = (sim_data[f'{sim_names[0]}']['Points:0'] - 0.33533) / 0.0064
 sim_end = np.where(sim_xd > 40 )[0][0]
-sim_var = 'UMean:0'
+sim_var = 'UMean:1'
 
 
 # Plotting
 # ----------------------------------------------------------------------------     
-colormap = ["#e60049", "#0bb4ff", "#50e991", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff", "#00bfa0"]
-linetype = ["-", "--", ":", "-."]
-legend = ['Experimental PIV']
-styles = plt.style.available
+# legend = ['Experimental PIV']
+legend = []
+plt.clf()
+# styles = plt.style.available
+# print(styles)
+bright = sns.color_palette("bright")
+sns.set_palette(bright)
+plt.style.use('seaborn-v0_8-bright')
+plt.figure(dpi=1000)
+fig, ax = plt.subplots()
+
 
 
 # Plot data
-plt.figure(1)
-plt.plot(exp_xd[exp_start:exp_end], exp_data[f'{exp_var}'][exp_start:exp_end], color=colormap[0], linewidth=1)
+# ax.plot(exp_xd[exp_start:exp_end-4], exp_data[f'{exp_var}'][0][exp_start:exp_end], linestyle='--', linewidth=0.8 )
 i = 1
 for name in sim_names:
-    plt.plot(sim_xd[:sim_end], sim_data[f'{name}'][f'{sim_var}'][:sim_end], color=colormap[i], linewidth=0.8)
+    ax.plot(sim_xd[:sim_end], sim_data[f'{name}'][f'{sim_var}'][:sim_end], color=bright[i], linewidth=1.25)
     legend += [f'CFD, k-omega SST {name}']
     i += 1        
 
 # Plot settings
-plt.grid(True)
-plt.grid(True, which='minor')
-plt.xlim([0, 40])
-plt.ylabel('$U_x$', fontsize=14)
-plt.xlabel('Axial Position $x/D_{jet}$', fontsize=14)
-plt.title('Axial Velocity Profile', fontsize=14)
-plt.legend(legend,fontsize=14)
+ax.grid(True, which='minor')
+ax.set_xlim([0, 25])
+ax.set_ylabel('$U_y$\t\n [m/s]', fontsize=12, labelpad=10)
+ax.yaxis.label.set(rotation='horizontal', ha='right');
+ax.set_xlabel('Axial Position $x/D_{jet}$', fontsize=12, labelpad=10)
+ax.set_title('Centreline Velocity Profile', fontsize=14, pad=10)
+ax.legend(legend,fontsize=10)
 
-for style in styles:
-    plt.style.use(style)
-    plt.show()
+plt.show()
 
+fig.savefig('C:\\Users\\drewm\\Documents\\2.0 MSc\\2.0 Simulations\\Figures\\Python\\Grid Independence\\Uy_M1-4.png',
+        dpi=1000 ,bbox_inches='tight', pad_inches=0.15)
