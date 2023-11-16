619b380684742b76147cb5ce669eda64fc163148
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-11-16 15:48:42 -0700

    adding secondary velocity calc with histogram method

--- Analysis/PIV_compare.py
+++ Analysis/PIV_compare.py
@@ -28,6 +28,8 @@ sim_locs = ['PIV_JetSim_M4']
 sim_names = ['kw']
 files = os.listdir(dir_home + sim_locs[0])
 all_pos = []
+r_tube = 0.025396
+U2 = {}
 
 sim_data = {}
 
@@ -39,20 +41,44 @@ for sim in sim_names:
         if name[:11] == 'radial_data':
             cur_pos = name[15:-4]
             all_pos.append(cur_pos)
+            # Update DF to include data for current radial postion
             sim_data[sim].update({ cur_pos : pd.read_csv(dir_home + sim_locs[0] + '\\' + name) })
-            # # Add normalized data, currently on ly for velocity. Will calc excess velocity , RNorm and such in another script
+            
+            # Add normalized velocity data
+            u_jet = np.max(sim_data[sim][cur_pos]['UMean:0'])
+            r_dimless = sim_data[sim][cur_pos]['Points:1'] / r_tube
+            
             if 'UNorm:0' not in sim_data[sim][cur_pos]:
-                Ux_norm = sim_data[sim][cur_pos]['UMean:0']/np.max(sim_data[sim][cur_pos]['UMean:0'])
-                Uy_norm = sim_data[sim][cur_pos]['UMean:1']/np.max(sim_data[sim][cur_pos]['UMean:0'])
-                tke_norm = sim_data[sim][cur_pos]['kMean']/(np.max(sim_data[sim][cur_pos]['UMean:0'])**2)
+                Ux_norm = sim_data[sim][cur_pos]['UMean:0'] / u_jet 
+                Uy_norm = sim_data[sim][cur_pos]['UMean:1']/ u_jet
+                tke_norm = sim_data[sim][cur_pos]['kMean']/ u_jet**2
                 sim_data[sim][cur_pos]['UNorm:0'] = Ux_norm
                 sim_data[sim][cur_pos]['UNorm:1'] = Uy_norm
                 sim_data[sim][cur_pos]['kNorm'] = tke_norm
                 sim_data[sim][cur_pos].to_csv(f'{dir_home}{sim_locs[0]}\\{name}',index=False)
-                ## Add r and theta calcs for points and velocities, from axial calcs
+                
+            # Calculate secondary and excess velocities
+            if 'U_secondary' not in sim_data[sim][cur_pos] and cur_pos != '0.0':
+                # Trim the wall effects to find the secondary velocity stream
+                y_trim_end = np.where(r_dimless > 0.85)[0][0]
+                hist, bins = np.histogram(sim_data[sim][cur_pos]['UMean:0'][:y_trim_end], bins=100) 
+                bin_idx = np.where(hist == np.max(hist[:90]))
+                U_secondary = bins[bin_idx[0]]
+                plt.hist(sim_data[sim][cur_pos]['UMean:0'][:y_trim_end], bins=100, edgecolor='black')
+                plt.show()
+                print(f'{cur_pos}:\t {U_secondary}')
+                U2.update({ cur_pos : U_secondary[0] })
+                sim_data[sim][cur_pos]['U_secondary'] = U_secondary[0]
+                test = 'jholder'
+               
+
+                
+                
+                
+
 
 # Select what positions and variable to plot        
-xd_pos = np.linspace(0, 30, 13)
+xd_pos = np.linspace(15, 30, 7)
 r_dimless = sim_data['kw']['0.0']['Points:1']/np.max(sim_data['kw']['0.0']['Points:1'])
 sim_var = 'UMean:0'
 
@@ -72,8 +98,8 @@ mpl.rcParams['font.family'] = 'book antiqua'
 # PLOT DATA
 fig, ax = plt.subplots()
 for pos in xd_pos:
-    ax.plot(r_dimless, sim_data[f'{pos}'][sim_var])
-    legend.append(f'xd = {pos}')  
+    ax.plot(r_dimless, sim_data['kw'][f'{pos}'][sim_var], linewidth=0.85)
+    legend.append('$x/D_{jet}$' + f' = {pos}')  
 
 ax.grid(True, which='minor')
 # ax.set_xlim([0, 25])
@@ -84,5 +110,5 @@ ax.set_xlabel('Radial Position: $r/R_{tube}$', fontsize=12, labelpad=10)
 ax.set_title('Radial Velocity Profile', fontsize=14, pad=12)
 ax.legend(legend,fontsize=10)
 
-fig.savefig('C:\\Users\\drewm\\Documents\\2.0 MSc\\2.0 Simulations\\Figures\\Python\\Radial Iso\\Ux_M4.png',
+fig.savefig('C:\\Users\\drewm\\Documents\\2.0 MSc\\2.0 Simulations\\Figures\\Python\\test.png',
         dpi=1000 ,bbox_inches='tight', pad_inches=0.15)
